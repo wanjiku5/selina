34e625e207cf3185ab61bea26efbf4f169b10e69
wanjiku5 <awanjikunjeri@gmail.com>
2024-02-29 13:00:19 +0300

    first commit

---
+++ appp.py
@@ -0,0 +1,24 @@
+
+
+
+my_list = []
+
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.append(50)
+
+
+my_list.insert(1, 25)
+
+
+my_list.extend([60, 70, 80])
+
+
+my_list.pop()
+
+
+my_list.sort()
+
+index_of_30 = my_list.index(30)
+print("Index of 30 in my_list:", index_of_30)
